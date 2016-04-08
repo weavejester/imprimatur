2e3fd3d10b63f77c32e8cce0e3773350b3ba998b
James Reeves <jreeves@weavejester.com>
2016-04-08 23:28:40 +0100

    Remove automated testing
    Automatically testing visualisation is hard and probably not worth it
    compared to just eyeballing the example project.

--- test/imprimatur/core_test.cljs
+++
@@ -1,7 +0,0 @@
-(ns imprimatur.core-test
-  (:require [cljs.test :refer-macros [deftest is testing]]
-            [imprimatur.core]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
--- test/imprimatur/test_runner.cljs
+++
@@ -1,5 +0,0 @@
-(ns imprimatur.test-runner
-  (:require [doo.runner :refer-macros [doo-tests]]
-            [imprimatur.core-test]))
-
-(doo-tests 'imprimatur.core-test)
