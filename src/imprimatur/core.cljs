a4fc84fadb028d8af31b6ca80d6084b7b9f08210
James Reeves <jreeves@weavejester.com>
2016-04-09 16:12:52 +0100

    Add Renderable protocol

--- src/imprimatur/core.cljs
+++ src/imprimatur/core.cljs
@@ -1,5 +1,15 @@
 (ns imprimatur.core
   (:require [flupot.dom :as dom]))
 
-(defn render [x]
-  (dom/pre (pr-str x)))
+(defprotocol Renderable
+  (render [x]))
+
+(extend-protocol Renderable
+  nil
+  (render [_] (dom/code "nil"))
+  string
+  (render [x] (dom/code (pr-str x)))
+  number
+  (render [x] (dom/code (str x)))
+  boolean
+  (render [x] (dom/code (str x))))
