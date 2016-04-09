d2be4bce183391013aaddb0adac6d3778703ccb5
James Reeves <jreeves@weavejester.com>
2016-04-09 01:51:57 +0100

    Add example project

--- example/project.clj
+++ example/project.clj
@@ -1,12 +1,14 @@
-(defproject imprimatur "0.1.0-SNAPSHOT"
+(defproject imprimatur/example "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [org.clojure/clojurescript "1.7.228"]]
+                 [org.clojure/clojurescript "1.7.228"]
+                 [imprimatur "0.1.0-SNAPSHOT"]]
   :plugins [[lein-cljsbuild "1.1.3"]]
   :cljsbuild
   {:builds {:main {:source-paths ["src"]
                    :compiler {:output-to "target/main.js"
-                              :optimizations :whitespace}}}})
+                              :optimizations :whitespace
+                              :main example.core}}}})
---
+++ example/src/example/core.cljs
@@ -0,0 +1,3 @@
+(ns example.core
+  (:require [imprimatur.core :as imp]))
+
