094fcfab939fc3aa424f658919c9093dd247669b
James Reeves <jreeves@weavejester.com>
2016-04-10 04:15:12 +0100

    Add 'print' component function

--- example/src/example/core.cljs
+++ example/src/example/core.cljs
@@ -6,5 +6,5 @@
   ["foo" 2 #{:bar '(foo bar)} true nil {:foo "bar" :baz "quz"}])
 
 (br/mount
- (imp/render data {2 {'(foo bar) true}})
+ (imp/print {:root data, :open {2 {'(foo bar) true}}})
  (.getElementById js/document "main"))
--- src/imprimatur/core.cljs
+++ src/imprimatur/core.cljs
@@ -1,61 +1,76 @@
 (ns imprimatur.core
-  (:require [sablono.core :refer-macros [html]]))
+  (:refer-clojure :exclude [print])
+  (:require [brutha.core :as br]
+            [sablono.core :refer-macros [html]]))
 
-(defprotocol Renderable
-  (render [x open]))
+(defprotocol IRenderable
+  (-render [x state]))
+
+(defn render [state]
+  (-render (:root state) state))
+
+(def print
+  (br/component render))
 
 (def ^:private ellipses
   (html [:span.ellipses "..."]))
 
 (defn- render-coll [class opening content closing]
   (html
-   [:div {:class class} [:span.opening opening] content [:span.closing closing]]))
+   [:div {:class class}
+    [:span.opening opening]
+    content
+    [:span.closing closing]]))
 
-(defn- ordered-content [coll open]
+(defn- ordered-content [coll {:keys [open]}]
   (if open
     (html
      [:ol.content
-      (map-indexed (fn [i x] (html [:li {:key i} (render x (get open i))])) coll)])
+      (map-indexed
+       (fn [i x] (html [:li {:key i} (print {:root x :open (get open i)})]))
+       coll)])
     ellipses))
 
-(defn- unordered-content [coll open]
+(defn- unordered-content [coll {:keys [open]}]
   (if open
     (html
      [:ul.content
       (for [x coll]
-        [:li {:key (pr-str x)} (render x (get open x))])])
+        [:li {:key (pr-str x)} (print {:root x :open (get open x)})])])
     ellipses))
 
-(defn- map-content [m open]
+(defn- map-content [m {:keys [open]}]
   (if open
     (html
      [:dl.content
       (for [[k v] m]
         (list
-         [:dt {:key (str "key$" (pr-str k))} (render k (get open k))]
-         [:dd {:key (str "val$" (pr-str v))} (render v (get open k))]))])
+         [:dt {:key (str "key$" (pr-str k))}
+          (print {:root k :open (:key (get open k))})]
+         [:dd {:key (str "val$" (pr-str v))}
+          (print {:root v :open (:val (get open k))})]))])
     ellipses))
 
-(extend-protocol Renderable
+(extend-protocol IRenderable
   nil
-  (render [_ _] (html [:code "nil"]))
+  (-render [_ _] (html [:code "nil"]))
   string
-  (render [x _] (html [:code (pr-str x)]))
+  (-render [x _] (html [:code (pr-str x)]))
   number
-  (render [x _] (html [:code (str x)]))
+  (-render [x _] (html [:code (str x)]))
   boolean
-  (render [x _] (html [:code (str x)]))
+  (-render [x _] (html [:code (str x)]))
   cljs.core.Symbol
-  (render [x _] (html [:code (str x)]))
+  (-render [x _] (html [:code (str x)]))
   cljs.core.Keyword
-  (render [x _] (html [:code (str x)]))
+  (-render [x _] (html [:code (str x)]))
   cljs.core.List
-  (render [xs open] (render-coll "list" "(" (ordered-content xs open) ")"))
+  (-render [xs state] (render-coll "list" "(" (ordered-content xs state) ")"))
   cljs.core.PersistentVector
-  (render [xs open] (render-coll "vector" "[" (ordered-content xs open) "]"))
+  (-render [xs state] (render-coll "vector" "[" (ordered-content xs state) "]"))
   cljs.core.PersistentHashSet
-  (render [xs open] (render-coll "set" "#{" (unordered-content xs open) "}"))
+  (-render [xs state] (render-coll "set" "#{" (unordered-content xs state) "}"))
   cljs.core.PersistentHashMap
-  (render [m open] (render-coll "map" "{" (map-content m open) "}"))
+  (-render [m state] (render-coll "map" "{" (map-content m state) "}"))
   cljs.core.PersistentArrayMap
-  (render [m open] (render-coll "map" "{" (map-content m open) "}")))
+  (-render [m state] (render-coll "map" "{" (map-content m state) "}")))
