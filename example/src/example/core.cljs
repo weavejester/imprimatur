95a36a05f5d74b828cc929c58c2f2a406f4c1621
James Reeves <jreeves@weavejester.com>
2016-04-12 03:19:28 +0100

    Add on-click toggle to example project

--- example/src/example/core.cljs
+++ example/src/example/core.cljs
@@ -5,6 +5,28 @@
 (def data
   ["foo" 2 #{:bar '(foo bar)} true nil {:foo "bar" :baz "quz"}])
 
-(br/mount
- (imp/print {:root data, :open {2 {'(foo bar) true}}})
- (.getElementById js/document "main"))
+(def open
+  (atom {}))
+
+(def main
+  (.getElementById js/document "main"))
+
+(defn toggle-open [m ks]
+  (if (get-in m ks)
+    (if (next ks)
+      (update-in m (butlast ks) dissoc (last ks))
+      (if-let [k (first ks)]
+        (dissoc m (first ks))))
+    (assoc-in (or m {}) ks {})))
+
+(defn render []
+  (br/mount
+   (imp/print
+    {:root data
+     :open @open
+     :on-click #(swap! open toggle-open %)})
+   main))
+
+(add-watch open :change (fn [_ _ _ _] (render)))
+
+(render)
