f4768a818a8a2c4e4e2e5ac68370ae738920ef51
James Reeves <jreeves@weavejester.com>
2016-04-15 23:33:18 +0100

    Add description and URL to project file

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject imprimatur "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Data visualization library for ClojureScript and React"
+  :url "https://github.com/weavejester/imprimatur"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
